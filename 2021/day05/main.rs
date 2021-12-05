1420674614ec2d4399daa3d0b09644b9250fd415
Marek Fajkus <marek.faj@gmail.com>
2021-12-05 12:23:37 +0100

    day05 part2

--- 2021/day05/main.rs
+++ 2021/day05/main.rs
@@ -1,10 +1,10 @@
+use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
-use std::cmp::{min,max};
 
 // Like vec2
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Point {
     x: i32,
     y: i32,
@@ -41,12 +41,17 @@ impl Plane {
     }
 
     fn add_line(&mut self, (p1, p2): (Point, Point)) {
-        let (start_x, end_x) = (min(p1.x, p2.x), max(p1.x,p2.x));
-        let (start_y, end_y) = (min(p1.y, p2.y), max(p1.y,p2.y));
-        for x in start_x..=end_x {
-            for y in start_y..=end_y {
-                self.add_point(Point { x, y })
-            }
+        let (start_x, end_x) = (min(p1.x, p2.x), max(p1.x, p2.x));
+        let (start_y, end_y) = (min(p1.y, p2.y), max(p1.y, p2.y));
+        let len = max(end_x - start_x, end_y - start_y);
+        let change_x = (p2.x - p1.x)/len;
+        let change_y = (p2.y - p1.y)/len;
+
+        for i in 0..=len {
+            self.add_point(Point{
+                x: p1.x + change_x*i,
+                y: p1.y + change_y*i,
+            })
         }
     }
 
@@ -83,18 +88,25 @@ fn main() {
     let lines: Lines<BufReader<File>> = BufReader::new(file).lines();
 
     let mut plane1: Plane = Plane::new();
+    let mut plane2: Plane = Plane::new();
 
     for (_, entry_) in lines.enumerate() {
         let entry = entry_.unwrap();
         let mut map = entry.split(" -> ").map(|v| Point::from(v.to_string()));
 
         let (p1, p2): (Point, Point) = (map.next().unwrap(), map.next().unwrap());
+        let line = (p1.clone(), p2.clone());
 
         if p1.x == p2.x || p1.y == p2.y {
-            plane1.add_line((p1,p2));
+            plane1.add_line(line.clone());
+            plane2.add_line(line);
+        } else if (p1.x - p2.x).abs() == (p1.y - p2.y).abs() {
+            plane2.add_line(line);
         }
     }
 
-    println!("plane1:\n{}", plane1.draw(250));
+    println!("plane1:\n{}", plane1.draw(10));
     println!("part1: {}", plane1.count_at_least(2));
+    println!("plane2:\n{}", plane2.draw(10));
+    println!("part2: {}", plane2.count_at_least(2));
 }
