5e007d696a9dc8e9e57ec2d5c9f0dbf1cc0bbb94
Marek Fajkus <marek.faj@gmail.com>
2021-12-07 08:38:41 +0100

    day07: both

---
+++ 2021/day07/main.rs
@@ -0,0 +1,58 @@
+use std::cmp::{min,max};
+use std::error::Error;
+use std::fs;
+
+fn align(input: Vec<i32>) -> i32 {
+    let mut lowest_fuel = i32::MAX;
+
+    for item in 0..*input.iter().max().unwrap() {
+        let position = item;
+        let mut fuel = 0;
+
+        for item in input.iter() {
+            fuel += (item - position).abs();
+
+            if fuel > lowest_fuel {
+                break;
+            }
+        }
+
+        lowest_fuel = min(lowest_fuel, fuel);
+    }
+
+    lowest_fuel
+}
+
+fn align2(input: Vec<i32>) -> i32 {
+    let mut lowest_fuel = i32::MAX;
+
+    for item in 0..*input.iter().max().unwrap() {
+        let position = item;
+        let mut fuel: i32 = 0;
+
+        for item in input.iter() {
+            let val: i32 = (item - position).abs();
+            fuel += (1..=val).sum::<i32>();
+
+            if fuel > lowest_fuel {
+                break;
+            }
+        }
+
+        lowest_fuel = min(lowest_fuel, fuel);
+    }
+
+    lowest_fuel
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let init: Vec<i32> = fs::read_to_string("day07/input.txt")?
+        .split(",")
+        .filter_map(|v| v.trim().parse().ok())
+        .collect();
+
+    println!("part1: {:?}", align(init.clone()));
+    println!("part2: {:?}", align2(init));
+
+    Ok(())
+}
